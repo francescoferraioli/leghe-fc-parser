4504cc874f21265a8b34eba8e7bfbb69fecb25d9
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2021-09-28 18:13:57 +1000

    Add parse for teams

---
+++ src/teams/parse.sh
@@ -0,0 +1,5 @@
+chromium "https://leghe.fantacalcio.it/ziero-tituli/rose"
+cat src/teams/parse.js | pbcopy
+sh -c "read -p 'Open dev tools, paste code and enter. Press enter when done.'"
+src/teams/save.sh
+src/teams/copy.sh
