56763ade90de42f9e5db963009d09e9f5f8314c6
Francesco Ferraioli <francesco.ferraioli@outlook.com>
2022-04-22 20:26:33 +1000

    Get the giornata from the html instead of arg

--- src/formations/parse.sh
+++ src/formations/parse.sh
@@ -1,5 +1,6 @@
 util/start-chrome "https://www.fantacalcio.it/probabili-formazioni-serie-a"
 cat src/formations/parse.js | pbcopy
 sh -c "read -p 'Open dev tools, paste code and enter. Press enter when done.'"
-src/formations/save.sh $1
-src/formations/copy.sh $1
+GIORNATA=$(curl --silent https://www.fantacalcio.it/probabili-formazioni-serie-a | rg ".*SERIE A - (\d+).* giornata.*" -r "\$1")
+src/formations/save.sh $GIORNATA
+src/formations/copy.sh $GIORNATA
